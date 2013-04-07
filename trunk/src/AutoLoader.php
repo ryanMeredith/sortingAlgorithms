7c828fad8a9bc912ce9dfd01b5ea01c92bf7b492
unknown <Borris@.(none)>
2013-04-07 11:47:55 +0100

    auto loader now uses namespaces, not as cool as itterators but will avoid namespace clashes

--- trunk/src/AutoLoader.php
+++ trunk/src/AutoLoader.php
@@ -1,32 +1,15 @@
 <?php
 
+
 class AutoLoader
 {
     public static function autoLoad($className)
     {
-		$maps = self::getDirectoryMap(dirname(__FILE__));
-		$filename = $className . ".php";
-		if (array_key_exists($filename, $maps)) {
-			require_once $maps[$filename];
+		$filename = dirname(__FILE__). "/".  str_replace('\\', '/', $className) . '.php';
+		if (is_readable($filename)) {
+			require_once $filename;
 		}
     }	
-
-	protected function getDirectoryMap( $path = '.')
-	{
-		$map = array();
-		$dir = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::FOLLOW_SYMLINKS);
-		$files = new RecursiveIteratorIterator(
-			new RecursiveRegexIterator($dir, '#^(?:(?!\.svn).)*(\.*/\w*|.php)$#i'),
-			true
-		);
-		
-		foreach ($files as $fileinfo) {
-			if (!$fileinfo->isDir()) {
-				$map[$fileinfo->getFilename()] = $fileinfo->getPathname();
-			}
-		}
-		return $map;
-	}
 	
 }
 spl_autoload_register(array("AutoLoader", "autoload"));
