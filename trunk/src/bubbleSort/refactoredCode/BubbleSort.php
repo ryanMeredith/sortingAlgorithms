3dfed397efffec3813777be86b6719d5d5634340
unknown <Borris@.(none)>
2013-04-08 23:55:30 +0100

    it is ugly but the bubble sort works, needs refactoring

--- trunk/src/bubbleSort/refactoredCode/BubbleSort.php
+++ trunk/src/bubbleSort/refactoredCode/BubbleSort.php
@@ -1,22 +1,12 @@
-<?php
+<?php namespace bubbleSort\refactoredCode;
 
 class BubbleSort
-{
-
-	protected $numberOfItems;
-
-	protected function setNumberOfItemsInArray($arrayToSort){
-		$this->numberOfItems = count($arrayToSort);
+{	
+	protected function needsSwapping($a, $b){
+		$needsSwapping = FALSE;
+		if($b < $a){
+			$needsSwapping = TRUE;
+		}
+		return $needsSwapping;
 	}
-	
-	protected function needsSwapping(){
-	
-	}
-	
-	protected function swapItemPositions(){
-	
-	}
-	
-	
-
 }
